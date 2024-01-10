2c8ea09fda7cd3f5c892536e25ff8dcc1f1305b3
Russell Matney <russell.matney@gmail.com>
2024-01-10 13:40:53 -0500

    feat: resizeSprite with lockRatio, then crop back down
    Won't work for all sizes, but it's working for my current base image and
    the main-capsule. Ready to run for all of them!

--- scripts/resize_canvas.lua
+++ scripts/resize_canvas.lua
@@ -5,24 +5,37 @@ function change_sprite_size(w, h)
   local s = app.sprite
   print("current sprite", s, s.width, s.height, s.pixelRatio)
 
-  -- too basic (loses aspect ratio)
-  -- s:resize(w, h)
-  -- print("resized sprite", s, s.width, s.height)
+  local longer = w
 
-  -- TODO handle w/h smaller than current sprite
+  if (w < h) then
+    longer = h
+  end
 
-  local extra_w = w - s.width
-  local extra_h = h - s.height
+  -- resize to larger dimension
+  app.command.SpriteSize({ui=false, width=longer, height=longer, lockRatio=true})
+  print("resized sprite", s.width, s.height)
 
-  local left = extra_w / 2
-  local right = extra_w / 2
-  local top = extra_h / 2
-  local bottom = extra_h / 2
+  -- crop shorter dimension
+  local new_x = 0
+  local new_y = 0
+  local new_width = s.width
+  local new_height = s.height
 
-  app.command.CanvasSize({ui=false, left=left, right=right, top=top, bottom=bottom})
+  if (w > h) then
+    local extra_h = s.height - h
+    new_y = extra_h / 2
+    new_height = h
+  else
+    local extra_w = s.width - w
+    new_x = extra_w / 2
+    new_width = w
+  end
 
-  print("resized canvas")
+  s:crop(new_x, new_y, new_width, new_height)
+  print("cropped sprite", s.width, s.height)
+
   s:saveAs(s.filename)
+  print("sprite re-saved")
 end
 
 -- main --------------------------------------------------------------
