aab510762943251671a718e70020022e095a7e4b
Russell Matney <russell.matney@gmail.com>
2024-01-10 13:17:09 -0500

    feat: refactor into proper canvas resize
    Now extending the canvas without modifying the sprite at all.

--- scripts/resize_canvas.lua
+++ scripts/resize_canvas.lua
@@ -3,11 +3,25 @@
 
 function change_sprite_size(w, h)
   local s = app.sprite
-  print("current sprite", s, s.width, s.height)
+  print("current sprite", s, s.width, s.height, s.pixelRatio)
 
-  s:resize(w, h)
+  -- too basic (loses aspect ratio)
+  -- s:resize(w, h)
+  -- print("resized sprite", s, s.width, s.height)
 
-  print("resized sprite", s, s.width, s.height)
+  -- TODO handle w/h smaller than current sprite
+
+  local extra_w = w - s.width
+  local extra_h = h - s.height
+
+  local left = extra_w / 2
+  local right = extra_w / 2
+  local top = extra_h / 2
+  local bottom = extra_h / 2
+
+  app.command.CanvasSize({ui=false, left=left, right=right, top=top, bottom=bottom})
+
+  print("resized canvas")
   s:saveAs(s.filename)
 end
 
