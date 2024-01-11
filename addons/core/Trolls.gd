fac0c8edddd43a1d61abc7128c074a0f523aacab
Russell Matney <russell.matney@gmail.com>
2024-01-11 13:26:05 -0500

    feat: game playable again!
    Moves Trolls to read ui_* inputs for movement rather than creating our
    own - i think this is more typical.

--- addons/core/Trolls.gd
+++ addons/core/Trolls.gd
@@ -24,7 +24,7 @@ static func is_released(event, event_name):
 # returns a normalized Vector2 based checked the controller's movement
 static func move_vector():
 	if Dino.focused:
-		return Input.get_vector("move_left", "move_right", "move_up", "move_down")
+		return Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	return Vector2.ZERO
 
 static func grid_move_vector(thresh=0.6):
@@ -40,18 +40,18 @@ static func grid_move_vector(thresh=0.6):
 	return Vector2.ZERO
 
 static func is_move(event):
-	return is_event(event, "move_left") or is_event(event, "move_right") or \
-		is_event(event, "move_up") or is_event(event, "move_down")
+	return is_event(event, "ui_left") or is_event(event, "ui_right") or \
+		is_event(event, "ui_up") or is_event(event, "ui_down")
 
 static func is_move_released(event):
-	return is_released(event, "move_left") or is_released(event, "move_right") or \
-		is_released(event, "move_up") or is_released(event, "move_down")
+	return is_released(event, "ui_left") or is_released(event, "ui_right") or \
+		is_released(event, "ui_up") or is_released(event, "ui_down")
 
 static func is_move_up(event):
-	return is_event(event, "move_up")
+	return is_event(event, "ui_up")
 
 static func is_move_down(event):
-	return is_event(event, "move_down")
+	return is_event(event, "ui_down")
 
 static func is_restart(event):
 	return is_event(event, "restart")
@@ -63,37 +63,7 @@ static func is_restart_released(event):
 	return is_released(event, "restart")
 
 static func is_undo(event):
-	return is_event(event, "undo")
-
-static func is_fire(event):
-	return is_event(event, "fire")
-
-static func is_fire_released(event):
-	return is_released(event, "fire")
-
-static func is_jump(event):
-	return is_event(event, "jump")
-
-static func is_dash(event):
-	return is_event(event, "dash")
-
-static func is_jetpack(event):
-	return is_event(event, "jetpack")
-
-static func is_attack(event):
-	return is_event(event, "attack")
-
-static func is_attack_released(event):
-	return is_released(event, "attack")
-
-static func is_action(event):
-	return is_event(event, "action")
-
-static func is_cycle_next_action(event):
-	return is_event(event, "cycle_next_action")
-
-static func is_cycle_prev_action(event):
-	return is_event(event, "cycle_previous_action")
+	return is_event(event, "ui_undo")
 
 static func is_pause(event):
 	return is_event(event, "pause")
