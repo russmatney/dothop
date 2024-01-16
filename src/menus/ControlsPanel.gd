ce14ebad56ea46986421494a478413beb4f1746d
Russell Matney <russell.matney@gmail.com>
2024-01-16 13:51:11 -0500

    feat: basic (tho buggy) control editing in place

--- src/menus/ControlsPanel.gd
+++ src/menus/ControlsPanel.gd
@@ -1,19 +1,26 @@
 @tool
 extends CanvasLayer
 
-@onready var edit_action_scene = preload("res://src/menus/EditActionRow.tscn")
+## vars ###############################################3
 
+@onready var edit_action_scene = preload("res://src/menus/EditActionRow.tscn")
 @onready var action_rows = $%EditActionRows
+@onready var reset_all_button = $%ResetButton
 
 var displayed_actions = [
 	"ui_accept", "ui_undo", "pause", "close", "restart",
 	"ui_left", "ui_right", "ui_up", "ui_down"
 	]
 
+## ready ###############################################3
+
 func _ready():
 	Log.pr("displayed actions", displayed_actions)
 
 	render_action_rows()
+	reset_all_button.pressed.connect(on_reset_all_pressed)
+
+## render ###############################################3
 
 func render_action_rows():
 	U.remove_children(action_rows)
@@ -21,3 +28,10 @@ func render_action_rows():
 		var row = edit_action_scene.instantiate()
 		row.action_name = action
 		action_rows.add_child(row)
+
+## reset all ###############################################3
+
+func on_reset_all_pressed():
+	Log.pr("resetting all actions!")
+	InputHelper.reset_all_actions()
+	render_action_rows()
--- src/menus/EditActionRow.gd
+++ src/menus/EditActionRow.gd
@@ -1,17 +1,29 @@
 @tool
 extends HBoxContainer
 
+## vars ###############################################3
+
 @export var action_name = "ui_accept"
 
 @onready var action_name_label = $%ActionName
 @onready var action_inputs = $%ActionInputs
 
+@onready var edit_button = $%EditButton
+
 var input_icon_scene = preload("res://src/menus/ActionInputIcon.tscn")
+
+var waiting_for_new_input = false
+var current_key_input
+var current_joy_input
+
+## ready ###############################################3
 
 func _ready():
 	action_name_label.text = "[center]%s[/center]" % action_name
-
 	render_action_icons()
+	edit_button.pressed.connect(on_edit_pressed)
+
+## render action icons ###############################################3
 
 func render_action_icons():
 	var keyboard_inputs = InputHelper.get_keyboard_inputs_for_action(action_name)
@@ -20,21 +32,49 @@ func render_action_icons():
 	U.remove_children(action_inputs)
 	# TODO if no action icon is found, we ought to remove the icon
 
-	Log.pr("\n\nicons for", action_name)
 	for inp in keyboard_inputs:
+		if not current_key_input:
+			current_key_input = inp
 		var icon = input_icon_scene.instantiate()
 		var key_str_mods = OS.get_keycode_string(inp.get_keycode_with_modifiers())
 		icon.input_text = key_str_mods
 		action_inputs.add_child(icon)
 
-	Log.pr("joy inputs", joypad_inputs)
 	for inp in joypad_inputs:
 		var icon = input_icon_scene.instantiate()
 		if "axis" in inp:
 			icon.joy_axis = [inp.axis, inp.axis_value]
 		else:
-			var joy_button_idx = inp.button_index
-			Log.pr("joy_button_idx", InputHelper.guess_device_name(), joy_button_idx)
+			if not current_joy_input:
+				current_joy_input = inp
 			icon.joy_button = [InputHelper.guess_device_name(), inp.button_index]
 
 		action_inputs.add_child(icon)
+
+## edit signals ###############################################3
+
+func on_edit_pressed():
+	waiting_for_new_input = true
+	Log.pr("edit pressed, waiting for input!", action_name)
+	edit_button.text = "...listening for new key..."
+
+## listening for new key ###############################################3
+
+func _unhandled_input(event) -> void:
+	if not waiting_for_new_input: return
+
+	var accepted = false
+	if (event is InputEventKey or event is InputEventMouseButton) and event.is_pressed():
+		accept_event()
+		InputHelper.replace_keyboard_input_for_action(action_name, current_key_input, event, true)
+		accepted = true
+
+	if event is InputEventJoypadButton and event.is_pressed():
+		accept_event()
+		InputHelper.replace_joypad_input_for_action(action_name, current_joy_input, event, true)
+		accepted = true
+
+	if accepted:
+		render_action_icons()
+		edit_button.text = "Edit"
+		waiting_for_new_input = false
