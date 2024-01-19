df9aabac7550fd0eda796ea960d463575bb7ac1a
Russell Matney <russell.matney@gmail.com>
2024-01-19 11:29:51 -0500

    wip: mute buttons in controls

--- src/menus/DotHopPauseList.gd
+++ src/menus/DotHopPauseList.gd
@@ -16,12 +16,56 @@ var button_defs = [
 		label="Main Menu",
 		fn=Navi.nav_to_main_menu,
 	},
+	{
+		label="Mute Music",
+		hide_fn=func(): return DJ.muted_music,
+		fn=func():
+		DJ.toggle_mute_music(true)
+		reload(),
+	},
+	{
+		label="Unmute Music",
+		hide_fn=func(): return not DJ.muted_music,
+		fn=func():
+		DJ.toggle_mute_music(false)
+		reload(),
+	},
+	{
+		label="Mute Sound",
+		hide_fn=func(): return DJ.muted_sound,
+		fn=func():
+		DJ.toggle_mute_sound(true)
+		reload(),
+	},
+	{
+		label="Unmute Sound",
+		hide_fn=func(): return not DJ.muted_sound,
+		fn=func():
+		DJ.toggle_mute_sound(false)
+		reload(),
+	},
+	{
+		label="Unmute All",
+		hide_fn=func(): return not DJ.muted_sound or not DJ.muted_music,
+		fn=func():
+		DJ.mute_all(false)
+		reload(),
+	},
+	{
+		label="Mute All",
+		hide_fn=func(): return DJ.muted_sound and DJ.muted_music,
+		fn=func():
+		DJ.mute_all(true)
+		reload(),
+	},
 ]
 
-
-func _ready():
+func reload():
 	for def in button_defs:
 		add_menu_item(def)
+
+func _ready():
+	reload()
 
 	if Engine.is_editor_hint():
 		request_ready()
