c53521a693138a8487e1f4723a3d43ce00e5ae87
Russell Matney <russell.matney@gmail.com>
2024-01-22 13:40:52 -0500

    feat: basic position and scale tween for zoom effect

--- src/menus/worldmap/WorldMap.gd
+++ src/menus/worldmap/WorldMap.gd
@@ -8,11 +8,21 @@ extends Node2D
 		current_marker = marker
 		center_map()
 
-
 func center_map():
 	var pos = Vector2(0, 0)
 	if current_marker != null:
 		pos = current_marker.position * -1
 
-	# TODO animate
-	map.position = pos
+	var t = create_tween()
+	t.tween_property(map, "position", pos, 0.4)\
+		.set_trans(Tween.TRANS_CUBIC)\
+		.set_ease(Tween.EASE_IN_OUT)
+
+	if pos == Vector2(0, 0):
+		t.parallel().tween_property(self, "scale", Vector2.ONE, 0.4)\
+			.set_trans(Tween.TRANS_CUBIC)\
+			.set_ease(Tween.EASE_IN_OUT)
+	else:
+		t.parallel().tween_property(self, "scale", Vector2.ONE * 2.0, 0.4)\
+			.set_trans(Tween.TRANS_CUBIC)\
+			.set_ease(Tween.EASE_IN_OUT)
