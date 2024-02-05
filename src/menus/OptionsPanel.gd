bb51897105636d672f10caf67f2b768eb3de0834
Russell Matney <russell.matney@gmail.com>
2024-02-05 11:01:24 -0500

    refactor: break out and reuse ControlsPanel
    cleans up the options and pause menus. Drops quick-theme swapping for
    now.

--- src/menus/OptionsPanel.gd
+++ src/menus/OptionsPanel.gd
@@ -3,9 +3,6 @@ extends CanvasLayer
 
 ## vars ###############################################3
 
-@onready var edit_action_scene = preload("res://src/menus/EditActionRow.tscn")
-@onready var action_rows = $%EditActionRows
-@onready var reset_controls_button = $%ResetControlsButton
 @onready var main_menu_button = $%MainMenuButton
 @onready var reset_save_data_button = $%ResetSaveDataButton
 @onready var unlock_all_button = $%UnlockAllButton
@@ -13,19 +10,9 @@ extends CanvasLayer
 @onready var data_reset_conf = $%DataResetConfirmationDialog
 @onready var data_unlock_conf = $%DataUnlockConfirmationDialog
 
-var displayed_actions = [
-	"ui_accept", "ui_undo", "pause", "close", "restart",
-	# "ui_up", "ui_down", "ui_left", "ui_right",
-	]
-
 ## ready ###############################################3
 
 func _ready():
-	Log.pr("displayed actions", displayed_actions)
-
-	render_action_rows()
-	reset_controls_button.pressed.connect(on_reset_controls_pressed)
-
 	reset_save_data_button.pressed.connect(func(): data_reset_conf.show())
 	unlock_all_button.pressed.connect(func(): data_unlock_conf.show())
 	data_reset_conf.confirmed.connect(func():
@@ -36,22 +23,3 @@ func _ready():
 	main_menu_button.pressed.connect(func():
 		Navi.nav_to_main_menu())
 
-## render ###############################################3
-
-func render_action_rows():
-	U.remove_children(action_rows)
-	for action in displayed_actions:
-		var row = edit_action_scene.instantiate()
-		row.action_name = action
-		action_rows.add_child(row)
-
-	for row in action_rows.get_children():
-		row.set_focus()
-		break
-
-## reset controls ###############################################3
-
-func on_reset_controls_pressed():
-	Log.pr("resetting controls!")
-	InputHelper.reset_all_actions()
-	render_action_rows()
--- src/menus/PauseList.gd
+++ src/menus/PauseList.gd
@@ -7,56 +7,8 @@ var button_defs = [
 		fn=Navi.resume,
 	},
 	{
-		label="Options",
-		# TODO return from controls to whatever brought us here
-		# TODO OR refactor into a popup panel
-		fn=Navi.nav_to.bind("res://src/menus/OptionsPanel.tscn"),
-	},
-	{
 		label="Main Menu",
 		fn=Navi.nav_to_main_menu,
-	},
-	{
-		label="Mute Music",
-		hide_fn=func(): return DJ.muted_music,
-		fn=func():
-		DJ.toggle_mute_music(true)
-		reload(),
-	},
-	{
-		label="Unmute Music",
-		hide_fn=func(): return not DJ.muted_music,
-		fn=func():
-		DJ.toggle_mute_music(false)
-		reload(),
-	},
-	{
-		label="Mute Sound",
-		hide_fn=func(): return DJ.muted_sound,
-		fn=func():
-		DJ.toggle_mute_sound(true)
-		reload(),
-	},
-	{
-		label="Unmute Sound",
-		hide_fn=func(): return not DJ.muted_sound,
-		fn=func():
-		DJ.toggle_mute_sound(false)
-		reload(),
-	},
-	{
-		label="Unmute All",
-		hide_fn=func(): return not DJ.muted_sound or not DJ.muted_music,
-		fn=func():
-		DJ.mute_all(false)
-		reload(),
-	},
-	{
-		label="Mute All",
-		hide_fn=func(): return DJ.muted_sound and DJ.muted_music,
-		fn=func():
-		DJ.mute_all(true)
-		reload(),
 	},
 ]
 
