394792473bf849c0abfff7dce6bcc8c27427bd3d
Russell Matney <russell.matney@gmail.com>
2024-02-12 14:17:16 -0500

    feat: buttons for navigating between puzzle sets
    Also supporting starting on an arbitrarily selected puzzle.

--- src/menus/worldmap/WorldMapMenu.gd
+++ src/menus/worldmap/WorldMapMenu.gd
@@ -8,9 +8,14 @@ extends CanvasLayer
 @onready var puzzle_set_icon = $%PuzzleSetIcon
 @onready var puzzle_set_title = $%PuzzleSetTitle
 @onready var start_puzzle_set_button = $%StartPuzzleSetButton
+@onready var start_puzzle_n_label = $%StartPuzzleNLabel
 
-var current_puzzle_set = 0
+@onready var next_puzzle_set_button = $%NextPuzzleSetButton
+@onready var previous_puzzle_set_button = $%PreviousPuzzleSetButton
+
+var current_puzzle_set: PuzzleSet
 var current_puzzle_set_idx = 0
+var current_puzzle_index = 0
 @onready var puzzle_sets = Store.get_puzzle_sets()
 
 ## ready ################################################
@@ -20,9 +25,17 @@ func _ready():
 		SoundManager.play_music(Music.late_night_radio)
 		reset_map()
 
-	set_puzzle_set()
+	attempt_move_to_puzzle_set(0)
 
-	start_puzzle_set_button.pressed.connect(on_start_button_pressed)
+	start_puzzle_set_button.pressed.connect(start_selected_puzzle)
+	next_puzzle_set_button.pressed.connect(show_next_puzzle_set)
+	previous_puzzle_set_button.pressed.connect(show_previous_puzzle_set)
+
+func show_next_puzzle_set():
+	attempt_move_to_puzzle_set(1)
+
+func show_previous_puzzle_set():
+	attempt_move_to_puzzle_set(-1)
 
 	# TODO resume music when naving back to here
 # 	visibility_changed.connect(on_visibility_changed)
@@ -31,17 +44,38 @@ func _ready():
 # 	if not Engine.is_editor_hint() and get_children()[0].is_visible_in_tree():
 # 		SoundManager.play_music(Music.late_night_radio)
 
-func set_puzzle_set():
-	current_puzzle_set_idx = clamp(current_puzzle_set_idx, 0, len(puzzle_sets) - 1)
-	current_puzzle_set = puzzle_sets[current_puzzle_set_idx]
-	show_puzzle_set(current_puzzle_set)
+func attempt_move_to_puzzle_set(delta: int):
+	var next_puzzle_set_idx = current_puzzle_set_idx + delta
+	next_puzzle_set_idx = clamp(next_puzzle_set_idx, 0, len(puzzle_sets) - 1)
+	var next_puzzle_set = puzzle_sets[next_puzzle_set_idx]
+
+	if next_puzzle_set.is_unlocked():
+		current_puzzle_set_idx = next_puzzle_set_idx
+		current_puzzle_set = next_puzzle_set
+		show_puzzle_set(current_puzzle_set)
+	else:
+		Log.pr("Cannot move to puzzle_set until it is unlocked")
+
+func next_puzzle_set_unlocked():
+	var next_puzzle_set_idx = clamp(current_puzzle_set_idx + 1, 0, len(puzzle_sets) - 1)
+	if next_puzzle_set_idx == current_puzzle_set_idx:
+		return false
+	var next_puzzle_set = puzzle_sets[next_puzzle_set_idx]
+	return next_puzzle_set.is_unlocked()
+
+func previous_puzzle_set_unlocked():
+	var prev_puzzle_set_idx = clamp(current_puzzle_set_idx - 1, 0, len(puzzle_sets) - 1)
+	if prev_puzzle_set_idx == current_puzzle_set_idx:
+		return false
+	var prev_puzzle_set = puzzle_sets[prev_puzzle_set_idx]
+	return prev_puzzle_set.is_unlocked()
 
 @onready var game_scene = preload("res://src/puzzle/GameScene.tscn")
 
-func on_start_button_pressed():
+func start_selected_puzzle():
 	Navi.nav_to(game_scene, {setup=func(g):
-		g.puzzle_set = current_puzzle_set,
-		# TODO set puzzle_num to next puzzle num
+		g.puzzle_set = current_puzzle_set
+		g.puzzle_num = current_puzzle_index
 		})
 
 ## input ###################################################################
@@ -53,14 +87,9 @@ func _unhandled_input(event):
 				Navi.pause()
 				get_viewport().set_input_as_handled()
 
-		# handle moving around the UI
-
-		if Trolls.is_move_right(event):
-			current_puzzle_set_idx += 1
-			set_puzzle_set()
-		if Trolls.is_move_left(event):
-			current_puzzle_set_idx -= 1
-			set_puzzle_set()
+func on_level_icon_gui_input(event: InputEvent):
+	if Trolls.is_accept(event):
+		start_selected_puzzle()
 
 ## show puzzle set ################################################
 
@@ -79,13 +108,13 @@ func show_puzzle_set(puzzle_set):
 	start_puzzle_set_button.text = puzzle_set.get_display_name()
 	start_puzzle_set_button.grab_focus.call_deferred()
 
+	next_puzzle_set_button.set_disabled(!next_puzzle_set_unlocked())
+	previous_puzzle_set_button.set_disabled(!previous_puzzle_set_unlocked())
+
 	var theme = puzzle_set.get_theme()
 
 	# var label = puzzle_label.instantiate()
 	# label.text = "[center]%s[/center]" % (puzzle.idx + 1)
-
-	# TODO support moving the player icon among the levels
-	# (maybe show a level-shape preview/popup?)
 
 	var next_puzzle_icon
 
@@ -94,13 +123,17 @@ func show_puzzle_set(puzzle_set):
 	for i in range(len(puzzle_set.get_puzzles())):
 		var puzzle = puzzle_set.get_puzzle(i)
 		if not puzzle:
+			Log.warn("Unexpected missing puzzle index!", i, puzzle_set)
 			continue
 		var icon = TextureRect.new()
 		icon.set_custom_minimum_size(64.0 * Vector2.ONE)
+		icon.gui_input.connect(on_level_icon_gui_input)
+		icon.focus_entered.connect(move_level_cursor.bind(icon))
 		if puzzle_set.completed_puzzle(i):
 			icon.set_texture(theme.get_dotted_icon())
+			icon.set_focus_mode(Control.FOCUS_ALL)
 		elif puzzle_set.can_play_puzzle(i):
-			# TODO animate/tween
+			icon.set_focus_mode(Control.FOCUS_ALL)
 			icon.set_texture(theme.get_dot_icon())
 			next_puzzle_icon = icon
 		else:
@@ -113,21 +146,30 @@ func show_puzzle_set(puzzle_set):
 	puzzle_set_icon.modulate.a = 0.0
 
 	if next_puzzle_icon:
-		U.call_in(0.5, self, func():
-			Log.pr("changing positions", puzzle_set_icon.position, next_puzzle_icon.global_position,
-				is_instance_valid(next_puzzle_icon))
-			var time = 0.4
-			var t = create_tween()
-			t.tween_property(puzzle_set_icon, "modulate:a", 1.0, time)\
-				.set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
-			t.parallel().tween_property(puzzle_set_icon, "position", next_puzzle_icon.global_position, time)\
-				.set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+		start_puzzle_set_button.focus_neighbor_bottom = next_puzzle_icon.get_path()
+		U.call_in(0.4, self, func(): move_level_cursor(next_puzzle_icon, {no_show=true}))
 
-			var scale_tween = create_tween()
-			scale_tween.tween_property(puzzle_set_icon, "scale", 1.3*Vector2.ONE, time/2).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
-			scale_tween.tween_property(puzzle_set_icon, "scale", 0.8*Vector2.ONE, time/4).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
-			scale_tween.tween_property(puzzle_set_icon, "scale", 1.0*Vector2.ONE, time/4).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
-			)
+func move_level_cursor(icon, opts={}):
+	var idx = puzzle_list.get_children().find(icon)
+	current_puzzle_index = idx
+	start_puzzle_n_label.text = "[center]Start Puzzle %s" % str(idx + 1)
+
+	if opts.get("no_show", false):
+		puzzle_set_icon.modulate.a = 0.0
+		puzzle_set_icon.position = icon.global_position
+		return
+
+	var time = 0.4
+	var t = create_tween()
+	t.tween_property(puzzle_set_icon, "modulate:a", 1.0, time)\
+		.set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	t.parallel().tween_property(puzzle_set_icon, "position", icon.global_position, time)\
+		.set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+
+	var scale_tween = create_tween()
+	scale_tween.tween_property(puzzle_set_icon, "scale", 1.3*Vector2.ONE, time/2).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	scale_tween.tween_property(puzzle_set_icon, "scale", 0.8*Vector2.ONE, time/4).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	scale_tween.tween_property(puzzle_set_icon, "scale", 1.0*Vector2.ONE, time/4).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
 
 
 func reset_map():
