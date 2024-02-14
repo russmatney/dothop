dafc1c0f7f06c55bf1b948adf58affef3f920887
Russell Matney <russell.matney@gmail.com>
2024-02-13 20:26:01 -0500

    feat: selecting and rendering themed puzzles on click
    tho, the backgrounds are completely blocking the ui after load :/

--- src/menus/editor/PuzzleSetEditor.gd
+++ src/menus/editor/PuzzleSetEditor.gd
@@ -9,6 +9,8 @@ extends CanvasLayer
 @onready var puzzles_grid = $%PuzzlesGrid
 @onready var current_puzzle_label = $%CurrentPuzzleLabel
 @onready var current_puzzle_analysis_label = $%CurrentPuzzleAnalysisLabel
+@onready var puzzle_container = $%PuzzleContainer
+var puzzle_node
 
 ## ready ######################################################
 
@@ -35,8 +37,8 @@ func on_puzzle_set_button_pressed(ps: PuzzleSet):
 	# Log.pr("puzzle_set button pressed", ps)
 	select_puzzle_set(ps)
 
-func on_puzzle_button_pressed(p):
-	select_puzzle(p)
+func on_puzzle_button_pressed(ps: PuzzleSet, p):
+	select_puzzle(ps, p)
 
 ## select ######################################################
 
@@ -53,14 +55,38 @@ func select_puzzle_set(ps: PuzzleSet):
 		texture.set_texture_normal(dot_texture)
 		texture.set_texture_disabled(dotted_texture)
 
-		texture.pressed.connect(on_puzzle_button_pressed.bind(p))
+		texture.pressed.connect(on_puzzle_button_pressed.bind(ps, p))
 
 		puzzles_grid.add_child(texture)
 
-func select_puzzle(p):
+func select_puzzle(ps: PuzzleSet, p):
+	# TODO this feels like it should be a viewport, not a panel container
+	U.remove_children(puzzle_container)
+
 	Log.pr("puzzle selected", p)
 
 	# TODO pretty print whatever this type is
 	current_puzzle_label.text = str(p)
 	# TODO print puzzle solver data here
 	current_puzzle_analysis_label.text = str(p)
+
+	if puzzle_node != null:
+		puzzle_container.remove_child.call_deferred(puzzle_node)
+		puzzle_node.queue_free()
+		# is this a race case? or is it impossible?
+		await puzzle_node.tree_exited
+
+	var theme = ps.get_theme()
+
+	puzzle_node = DotHopPuzzle.build_puzzle_node({
+		puzzle_scene=theme.get_puzzle_scene(),
+		game_def=ps.get_game_def(),
+		puzzle_num=p.get("idx"),
+		})
+	# TODO simply by passing theme in above?
+	puzzle_node.player_scenes = theme.get_player_scenes()
+	puzzle_node.dot_scenes = theme.get_dot_scenes()
+	puzzle_node.goal_scenes = theme.get_goal_scenes()
+	# puzzle_node.hide_background = true
+
+	puzzle_container.add_child(puzzle_node)
