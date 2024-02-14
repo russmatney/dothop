741cf09604977f33aa858a03726dce697b7d5b1b
Russell Matney <russell.matney@gmail.com>
2024-02-13 20:05:29 -0500

    wip: logging the shape of the selected puzzle
    now, how best to render this thing? with the game scene of course!

--- src/menus/editor/PuzzleSetEditor.gd
+++ src/menus/editor/PuzzleSetEditor.gd
@@ -7,6 +7,8 @@ extends CanvasLayer
 
 @onready var puzzle_set_grid = $%PuzzleSetGrid
 @onready var puzzles_grid = $%PuzzlesGrid
+@onready var current_puzzle_label = $%CurrentPuzzleLabel
+@onready var current_puzzle_analysis_label = $%CurrentPuzzleAnalysisLabel
 
 ## ready ######################################################
 
@@ -27,18 +29,38 @@ func render():
 		button.pressed.connect(on_puzzle_set_button_pressed.bind(ps))
 		puzzle_set_grid.add_child(button)
 
-func on_puzzle_set_button_pressed(ps: PuzzleSet):
-	Log.pr("puzzle_set button pressed", ps)
+## on ######################################################
 
+func on_puzzle_set_button_pressed(ps: PuzzleSet):
+	# Log.pr("puzzle_set button pressed", ps)
 	select_puzzle_set(ps)
+
+func on_puzzle_button_pressed(p):
+	select_puzzle(p)
+
+## select ######################################################
 
 func select_puzzle_set(ps: PuzzleSet):
 	U.remove_children(puzzles_grid)
 	for p in ps.get_puzzles():
 		# var bg_music = ps.get_theme().get_background_music()
-		# var dot_texture = ps.get_theme().get_dot_icon()
-		# var dotted_texture = ps.get_theme().get_dotted_icon()
+		var dot_texture = ps.get_theme().get_dot_icon()
+		var dotted_texture = ps.get_theme().get_dotted_icon()
 		var player_texture = ps.get_theme().get_player_icon()
-		var texture = TextureRect.new()
-		texture.set_texture(player_texture)
+		var texture = TextureButton.new()
+
+		texture.set_texture_hover(player_texture)
+		texture.set_texture_normal(dot_texture)
+		texture.set_texture_disabled(dotted_texture)
+
+		texture.pressed.connect(on_puzzle_button_pressed.bind(p))
+
 		puzzles_grid.add_child(texture)
+
+func select_puzzle(p):
+	Log.pr("puzzle selected", p)
+
+	# TODO pretty print whatever this type is
+	current_puzzle_label.text = str(p)
+	# TODO print puzzle solver data here
+	current_puzzle_analysis_label.text = str(p)
