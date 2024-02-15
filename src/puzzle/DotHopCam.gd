9ca105be82cd3f2234ebf8360ca7137d026f9ef5
Russell Matney <russell.matney@gmail.com>
2024-02-15 15:56:41 -0500

    refactor: move backgrounds to fixed canvas layers

--- src/puzzle/DotHopCam.gd
+++ src/puzzle/DotHopCam.gd
@@ -1,13 +1,8 @@
 extends Camera2D
 
-func _ready():
-	Log.pr("dh cam ready")
-
 var node_rect
 var zoom_min = 0.5
 var zoom_max = 5.0
-
-# var zoom_margin = Vector4(-64, -64, 64, 64)
 var base_margin = 64
 
 func center_on_nodes(nodes):
@@ -23,32 +18,14 @@ func center_on_nodes(nodes):
 		else:
 			rect = rect.expand(node.get_global_position())
 
-	# rect = rect.grow_individual(zoom_margin.x, zoom_margin.y, zoom_margin.z, zoom_margin.w)
-
-	Log.pr("new node_rect", rect)
 	node_rect = rect
-	# queue_redraw()
 
 	rect = rect.grow(base_margin)
 
 	var screen_size: Vector2 = get_viewport_rect().size
-	Log.pr("screen size", screen_size)
 	if rect.size.x > rect.size.y * screen_size.aspect():
-		Log.pr("larger x", screen_size.x / rect.size.x)
 		zoom = clamp(screen_size.x / rect.size.x, zoom_min, zoom_max) * Vector2.ONE
 	else:
-		Log.pr("larger y", screen_size.y / rect.size.y)
 		zoom = clamp(screen_size.y / rect.size.y, zoom_min, zoom_max) * Vector2.ONE
-	Log.pr("clamped zoom", zoom)
 
 	global_position = rect.get_center()
-
-
-# func _draw():
-# 	if node_rect:
-# 		Log.pr("drawing node rect", node_rect)
-# 		draw_rect(node_rect, Color("3ab99a"), false, 2)
-
-# 		draw_circle(node_rect.get_center(), 5, Color("3ab99a"))
-# 	else:
-# 		Log.pr("no node rect!")
