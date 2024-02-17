f96a32328e51e8fbe3bffda9a16e642df9007404
Russell Matney <russell.matney@gmail.com>
2024-02-17 18:51:29 -0500

    wip: toying with lighting on the space theme

--- src/themes/space/Dot.gd
+++ src/themes/space/Dot.gd
@@ -5,6 +5,8 @@ extends DotsDot
 @onready var asteroid2 = $Asteroid2
 @onready var star = $Star
 
+@onready var lightOccluder = $LightOccluder2D
+
 ## ready ###########################################################
 
 func _ready():
@@ -16,7 +18,6 @@ func _ready():
 		DHData.dotType.Dot:
 			anim = U.rand_of([asteroid1, asteroid2])
 			anim.play("dot")
-			U.set_random_frame(anim)
 		DHData.dotType.Dotted:
 			anim = U.rand_of([asteroid1, asteroid2])
 			anim.play("dotted")
@@ -31,3 +32,15 @@ func _ready():
 			an.set_visible(false)
 		else:
 			an.set_visible(true)
+
+func render():
+	if lightOccluder:
+		match type:
+			DHData.dotType.Dot:
+				lightOccluder.set_occluder_light_mask(1)
+			DHData.dotType.Dotted:
+				lightOccluder.set_occluder_light_mask(0)
+			DHData.dotType.Goal:
+				lightOccluder.set_occluder_light_mask(1)
+
+	super.render()
