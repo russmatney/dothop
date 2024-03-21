244f54c4bfd7fa95ea13cf2c56ed3cfdf2090962
Russell Matney <russell.matney@gmail.com>
2024-03-21 18:55:53 -0400

    chore: update sound_manager and input_helper via gd-plug

--- plug.gd
+++ plug.gd
@@ -1,10 +1,10 @@
 extends "res://addons/gd-plug/plug.gd"
 
 func _plugging():
-	# Declare plugins with plug(repo, args)
-	# For example, clone from github repo("user/repo_name")
-	# plug("imjp94/gd-YAFSM") # By default, gd-plug will only install anything from "addons/" directory
-	# Or you can explicitly specify which file/directory to include
-	# plug("imjp94/gd-YAFSM", {"include": ["addons/"]}) # By default, gd-plug will only install anything from "addons/" directory
 	plug("MikeSchulze/gdUnit4")
 	plug("imjp94/gd-plug-ui")
+	# plug("bitbrain/pandora")
+	plug("nathanhoad/godot_input_helper", {include=["addons/input_helper"]})
+	plug("nathanhoad/godot_sound_manager", {include=["addons/sound_manager"]})
+	# plug("timothyqiu/gdfxr")
+	plug("viniciusgerevini/godot-aseprite-wizard", {include=["addons/AsepriteWizard"]})
