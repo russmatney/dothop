fb24ceb8608d566301032a34276c4663aab1ef87
Russell Matney <russell.matney@gmail.com>
2024-12-02 11:57:21 -0500

    chore: port Trolls tweaks to bones/Trolls

--- addons/bones/Trolls.gd
+++ addons/bones/Trolls.gd
@@ -20,24 +20,40 @@ static func is_released(event, event_name):
 		return event.is_action_released(event_name)
 	return false
 
+static func is_screen_drag_event(event):
+	if event is InputEventScreenDrag:
+		return (
+			(abs(event.relative.x) > 4 or abs(event.relative.y) > 4)
+			and
+			(abs(event.velocity.x) > 100 or abs(event.velocity.y) > 100))
 
-# returns a normalized Vector2 based on the controller's movement
-static func move_vector():
-	if Debug.focused:
+# returns a normalized Vector2 based checked the controller's movement
+static func move_vector(event=null):
+	if event:
+		if Trolls.is_screen_drag_event(event):
+			var dir = to_cardinal_direction(event.velocity, 100)
+			Log.pr("drag gesture dir", dir, "velocity", event.velocity, "relative", event.relative)
+			return dir
+	if Dino.focused:
 		return Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	return Vector2.ZERO
 
-static func grid_move_vector(thresh=0.6):
-	var move = move_vector()
-	if move.x > thresh:
+static func to_cardinal_direction(vec, thresh=0.6):
+	if abs(vec.y) > abs(vec.x):
+		if vec.y < -1*thresh:
+			return Vector2.UP
+		elif vec.y > thresh:
+			return Vector2.DOWN
+
+	if vec.x > thresh:
 		return Vector2.RIGHT
-	elif move.x < -1*thresh:
+	elif vec.x < -1*thresh:
 		return Vector2.LEFT
-	elif move.y < -1*thresh:
-		return Vector2.UP
-	elif move.y > thresh:
-		return Vector2.DOWN
 	return Vector2.ZERO
+
+static func grid_move_vector(event=null, thresh=0.6):
+	var move = move_vector(event)
+	return Trolls.to_cardinal_direction(move, thresh)
 
 static func is_move(event):
 	return is_event(event, "ui_left") or is_event(event, "ui_right") or \
@@ -52,6 +68,15 @@ static func is_move_up(event):
 
 static func is_move_down(event):
 	return is_event(event, "move_down")
+
+static func is_restart(event):
+	return is_event(event, "restart")
+
+static func is_restart_held(event):
+	return is_held(event, "restart")
+
+static func is_restart_released(event):
+	return is_released(event, "restart")
 
 static func is_undo(event):
 	return is_event(event, "undo")
