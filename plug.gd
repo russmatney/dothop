814d275d426bb956b57a9cab58bcaa2c17de2c9e
Russell Matney <russell.matney@gmail.com>
2025-05-28 10:38:12 -0400

    deps: update bones and gd-plug

--- plug.gd
+++ plug.gd
@@ -5,7 +5,7 @@ func _plugging() -> void:
 	plug("bitbrain/pandora", {include=["addons/pandora"]})
 	plug("nathanhoad/godot_input_helper", {include=["addons/input_helper"]})
 	plug("nathanhoad/godot_sound_manager", {include=["addons/sound_manager"]})
-	plug("timothyqiu/gdfxr")
+	plug("timothyqiu/gdfxr", {branch="godot-4"})
 	plug("viniciusgerevini/godot-aseprite-wizard", {include=["addons/AsepriteWizard"]})
 	plug("russmatney/log.gd", {include=["addons/log"]})
 	plug("russmatney/bones", {include=[
