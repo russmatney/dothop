0c127dbf0ff2dd4002dfa69a41e5afe149c127eb
Russell Matney <russell.matney@gmail.com>
2025-07-30 12:40:07 -0400

    fix: pull themes from ThemeStore

--- src/menus/ThemeButtonList.gd
+++ src/menus/ThemeButtonList.gd
@@ -1,11 +1,10 @@
 @tool
 extends ButtonList
 
-@onready var themes := Store.get_themes()
-
 func _ready() -> void:
+	var themes := ThemeStore.get_themes()
 	for th in themes:
 		add_menu_item({
-			label=th.get_display_name(),
-			fn=Events.puzzle_node.fire_change_theme.bind(th.get_theme_data()),
+			label=th.display_name,
+			fn=Events.puzzle_node.fire_change_theme.bind(th),
 			})
